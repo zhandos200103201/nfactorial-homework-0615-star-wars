9c67f6c47374fc4c37d55fa7b7c610bf627a9e25
Zhandos <saparbayevzh03@gmail.com>
2022-06-15 16:44:07 +0600

    Star-wars-nFactorial

---
+++ components/footer/index.jsx
@@ -0,0 +1,16 @@
+// import "./App.css";
+
+export const Footer = () => {
+    return (
+<div class="container">
+  <footer class="py-3 my-4">
+    <div class="nav justify-content-center border-bottom pb-3 mb-3">
+      <a href="https://www.starwars.com/" class="nav-link px-2 text-muted">Home</a>
+      <a href="https://www.starwars.com/films" class="nav-link px-2 text-muted">Films</a>
+      <a href="https://www.starwars.com/disneyplus" class="nav-link px-2 text-muted">Disnay+</a>
+    </div>
+    <p class="text-center text-muted">&copy; 2021 Company, Inc</p>
+  </footer>
+</div>
+    );
+};
---
+++ src/components/content/index.jsx
@@ -0,0 +1,63 @@
+import { useState } from "react";
+
+import swCharacters from "../../data/characters.json";
+import swPlanets from "../../data/planets.json";
+import spwStarships from "../../data/starships.json";
+
+import { ItemSpisok } from "../sw-item-spisok";
+
+import { ItemLists } from "../sw-item-lists";
+
+
+function setTochange(){
+  return <h1>Hello world!</h1>
+}
+
+export const Content = ({ handleCreateFan }) => {
+  const [selector, setSelector] = useState("Characters");
+
+  const handleSelectorChange = (event) => {
+    setSelector(event.target.value);
+  };
+
+  return (
+    <div className="content-layout">
+      <select
+        value={selector}
+        placeholder="Choose your path"
+        className="select-item"
+        onChange={handleSelectorChange}
+      >
+        <option>Characters</option>
+        <option>Planets</option>
+        <option>Starships</option>
+      </select>
+
+      {selector === "Characters" &&
+        swCharacters.map((character) => (
+          <ItemLists key={character.name} item={character} type="chars">
+            <ItemSpisok>Gender: {character.gender}</ItemSpisok>
+            <ItemSpisok>Birthday: {character.birth_year}</ItemSpisok>
+          </ItemLists>
+        ))}
+
+      {selector === "Planets" &&
+        swPlanets.map((planet) => (
+          <ItemLists key={planet.name} item={planet} type="planets">
+            <ItemSpisok>Rotation Period: {planet.rotation_period}</ItemSpisok>
+            <ItemSpisok>Orbital Period: {planet.orbital_period}</ItemSpisok>
+          </ItemLists>
+        ))}
+
+      {selector === "Starships" &&
+        spwStarships.map((starShips) => (
+        <ItemLists key={starShips.name} item={starShips} type="starShips">
+          <ItemSpisok>Model: {starShips.model}</ItemSpisok>
+          <ItemSpisok>Manufacturer: {starShips.manufacturer}</ItemSpisok>
+        </ItemLists>
+      ))}
+    
+    </div>
+  );
+};
+
